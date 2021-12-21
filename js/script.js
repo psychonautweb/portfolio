a1e1fbcf02eb681ceb71dc7b7110978d678912bf
Dubravko Cajic <mdcajic@yahoo.com>
2021-12-21 19:39:50 +0100

    finished portfolio section
    completed section. removed ss folder

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@
 const tabsContainer = document.querySelector('.about-tabs'), 
 aboutSection = document.querySelector('.about-section');
 
+// function that's switching between "education" and "experience" content
 tabsContainer.addEventListener('click', (e) => {
-    console.log(e.target);
+    if (e.target.classList.contains('tab-item') && !e.target.classList.contains('active')) {
+        tabsContainer.querySelector('.active').classList.remove('active');
+        e.target.classList.add('active');
+        const target = e.target.getAttribute('data-target');
+        aboutSection.querySelector('.tab-content.active').classList.remove('active');
+        aboutSection.querySelector(target).classList.add('active');
+    }
 })
