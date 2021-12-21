a7dd1cf08b204f922787e43cd1eac29296d1b911
Dubravko Cajic <mdcajic@yahoo.com>
2021-12-21 19:08:08 +0100

    updated script.js
    added tabsContainer and aboutSection const. Assigned:
    about-tabs to tabsContainer and about-section to aboutSection const

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+// About Tabs ------------------------------- //
+const tabsContainer = document.querySelector('.about-tabs'), 
+aboutSection = document.querySelector('.about-section');
 
+tabsContainer.addEventListener('click', (e) => {
+    console.log(e.target);
+})
